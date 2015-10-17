fcfe7ab153bfc77421f5914baca93dadfcdb7488
fafaro <furqanfarooqui@gmail.com>
2015-10-17 13:10:44 -0500

    Requirement analysis and playing with TypeScript

---
+++ test.js
@@ -0,0 +1,9 @@
+function greet(x) {
+    window.alert(x);
+}
+greet("Furqan");
+greet("Cute!");
+var x, y;
+x = 6;
+y = (6).toString();
+//# sourceMappingURL=test.js.map
---
+++ test.ts
@@ -0,0 +1,12 @@
+function greet(x : string) {
+	window.alert(x);
+}
+
+greet("Furqan");
+greet("Cute!");
+
+let x : number, y : string;
+x = 6;
+y = (6).toString();
+
+console.log(x);
