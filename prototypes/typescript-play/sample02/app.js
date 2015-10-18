709112e3b2cdfee07f2783711f2215817f93bbd7
fafaro <furqanfarooqui@gmail.com>
2015-10-18 01:35:01 -0500

    TypeScript play sample02

--- prototypes/typescript-play/sample02/app.js
+++ prototypes/typescript-play/sample02/app.js
@@ -1,3 +1,26 @@
+/// <reference path="jquery.d.ts"/>
+/// <reference path="d3.d.ts"/>
 $(function () {
-    $("#app").html("Hello World");
+    var svg = d3.select("#app")
+        .append("svg")
+        .attr("width", 50)
+        .attr("height", 50);
+    var data = new Array(10);
+    function updateBar(t) {
+        function height(i) {
+            return i * 5 + 3 * (1 + 1.2 * Math.sin(-i * 1.5 + t * 0.15));
+        }
+        svg.selectAll("rect")
+            .data(data)
+            .attr("y", function (d, i) { return 50 - height(i); })
+            .attr("height", function (d, i) { return height(i); })
+            .attr("fill", "blue")
+            .enter()
+            .append("rect")
+            .attr({ "x": function (d, i) { return i * 5; }, "width": 4 });
+    }
+    var t = 0;
+    window.setInterval(function () {
+        updateBar(t++);
+    }, 10);
 });
--- prototypes/typescript-play/sample02/app.ts
+++ prototypes/typescript-play/sample02/app.ts
@@ -1,6 +1,30 @@
 /// <reference path="jquery.d.ts"/>
+/// <reference path="d3.d.ts"/>
 
 $(function() {
-    $("#app").html("Hello World");
-    var e = document.getElementById("app");
+    let svg = d3.select("#app")
+        .append("svg")
+        .attr("width", 50)
+        .attr("height", 50);
+        
+    let data = new Array(10);
+        
+    function updateBar(t : number) {
+        function height(i : number) : number {
+            return i * 5 + 3 * (1 + 1.2 * Math.sin(-i * 1.5  + t * 0.15));
+        }
+        svg.selectAll("rect")
+            .data(data)
+            .attr("y", (d, i) => 50 - height(i))
+            .attr("height", (d,i) => height(i))
+            .attr("fill", "blue")
+            .enter()
+            .append("rect")
+            .attr({"x": (d, i) => i * 5, "width": 4});            
+    }
+
+    let t = 0;
+    window.setInterval(function() {
+        updateBar(t++);
+    }, 10);
 });
